2d48dc70d5c78d64882939198dbfa0873bb16796
Ian Fosbery <ian.fosbery@reaktor.com>
2016-08-31 14:27:17 -0400

    Only use GET

--- app.rb
+++ app.rb
@@ -3,27 +3,16 @@ require "sinatra"
 class ReaktorSlack < Sinatra::Base
   get "/thebeef" do
     content_type :json
-    get_the_beef
-  end
-
-  post "/thebeef" do
-    content_type :json
-    get_the_beef
-  end
-
-  #TODO: More is more, danger zone
-
-  private
-
-  def get_the_beef
-    { 
+    {
       response_type: "in_channel",
       attachments: [
         {
           text: "A standing f***ing ovation for you.",
           image_url: "https://s3.amazonaws.com/codeguy-slackbot/images/ezgif.com-optimize.gif"
         }
-      ] 
+      ]
     }.to_json
   end
+
+  #TODO: More is more, danger zone
 end
