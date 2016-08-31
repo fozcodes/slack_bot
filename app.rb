b5a480336ec65b479641f4d4aef1c60594647edb
Ian Fosbery <ian.fosbery@reaktor.com>
2016-08-30 23:19:45 -0400

    Fix response JSON to be public

--- app.rb
+++ app.rb
@@ -3,20 +3,22 @@ require "sinatra"
 class ReaktorSlack < Sinatra::Base
   get "/thebeef" do
     content_type :json
-    { 
-      attachments: [
-        {
-          image_url: "http://i.imgur.com/asQ6Ijt.gif"
-        }
-      ] 
-    }.to_json
+    get_the_beef
   end
 
   post "/thebeef" do
     content_type :json
+    get_the_beef
+  end
+
+  private
+
+  def get_the_beef
     { 
+      response_type: "in_channel",
       attachments: [
         {
+          text: "A standing f***ing ovation for you.",
           image_url: "http://i.imgur.com/asQ6Ijt.gif"
         }
       ] 
