ac5dc7519954c4721a26b49bc15bc4c4a9b13ae5
Ian Fosbery <ian.fosbery@reaktor.com>
2016-08-30 23:10:24 -0400

    Add post as well

--- app.rb
+++ app.rb
@@ -11,4 +11,15 @@ class ReaktorSlack < Sinatra::Base
       ] 
     }.to_json
   end
+
+  post "/thebeef" do
+    content_type :json
+    { 
+      attachments: [
+        {
+          image_url: "http://i.imgur.com/asQ6Ijt.gif"
+        }
+      ] 
+    }.to_json
+  end
 end
