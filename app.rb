49c9f391eebf555e3641fe2015bce331318de0ad
Ian Fosbery <ian.fosbery@reaktor.com>
2016-08-30 23:01:58 -0400

    Init commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+require "sinatra"
+
+class ReaktorSlack < Sinatra::Base
+  get "/thebeef" do
+    content_type :json
+    { 
+      attachments: [
+        {
+          image_url: "http://i.imgur.com/asQ6Ijt.gif"
+        }
+      ] 
+    }.to_json
+  end
+end
