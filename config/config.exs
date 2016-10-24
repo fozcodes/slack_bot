c730fe80d111fd1c1941acd98b7f185aec05469a
Ian Fosbery <ian.fosbery@reaktor.com>
2016-10-24 14:00:55 -0400

    Add dynamic port

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@
 use Mix.Config
 
 config :trot, :router, SlackBot.Router
+config :trot, :port, System.get_env("PORT") || 4000
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
